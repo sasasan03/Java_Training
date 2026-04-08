db5b5dedffe13f031b8d41b274545b1ebec098d5
sasasan03 <sako.hiro62@gmail.com>
2026-04-08 17:19:32 +0900

    update問題（指定idのレコードの上書き）

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,12 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 // 一斉リネーム: com + opt + R
 // 最後尾へ移動： com 矢印
 // 行を複製： com + opt ＋ 下
+// インデックスの整形：　com + shif + F
 
 public class Student {
 
@@ -14,20 +14,19 @@ public class Student {
 		String url = "jdbc:postgresql://localhost:5432/training";
 		String user = "student";
 		String password = "password";
-	
-		String selectIdSql = "SELECT * FROM student WHERE id = ?";
-		
-		try(
-				Connection conn = DriverManager.getConnection(url,user,password);
-				PreparedStatement ps = conn.prepareStatement(selectIdSql);
-			) {
-			ps.setInt(1, 100);
-			try(ResultSet rs = ps.executeQuery();) {
-				if (rs.next()) {
-						System.out.println(rs.getString("name"));
-				} else {
-					System.out.println("該当なし");
-				}
+
+		String updateSql = "UPDATE student SET name = ?, age = ? WHERE id = ?";
+
+		try (Connection conn = DriverManager.getConnection(url, user, password);
+				PreparedStatement ps = conn.prepareStatement(updateSql);) {
+			ps.setString(1, "谷本");
+			ps.setInt(2, 29);
+			ps.setInt(3, 100);
+			int count = ps.executeUpdate();
+			if (count != 0) {
+				System.out.println("作成カウント：" + count);
+			} else {
+				System.out.println("該当なし");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
