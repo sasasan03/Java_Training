c0e1a706e5c411e4a762443536962f8ca199a497
sasasan03 <sako.hiro62@gmail.com>
2026-04-08 16:24:40 +0900

    insert問題

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Student {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		String url = "jdbc:postgresql://localhost:5432/training";
+		String user = "student";
+		String password = "password";
+		Connection conn;
+		Statement statement;
+		
+		try {
+			conn =  DriverManager.getConnection(url,user,password);
+			System.out.println("===========データベース接続成功");
+			String sql = "INSERT INTO student (name, age) VALUES ('田中', 30)";
+		    PreparedStatement ps = conn.prepareStatement(sql);
+			ps.executeUpdate();
+			System.out.println("===========保存完了");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
